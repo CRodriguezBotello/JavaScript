1ed945fc056158f2bcd8a7cdabae9cb9434bc5f8
Carlos Rodriguez <crbotello08@gmail.com>
2024-10-16 09:04:51 +0200

    ejercicio6

---
+++ funciones/ej_6.js
@@ -0,0 +1,9 @@
+'use strict'
+
+let num1= Number(prompt("Introduce el primer numero"));
+let num2= Number(prompt("Introduce el segundo numero"));
+let num3= Number(prompt("Introduce el tercer numero"));
+
+let sol= (num1 + num2 + num3)/3;
+
+document.write("El promedio de "+num1+", "+num2+", "+num3+" es: "+sol);
