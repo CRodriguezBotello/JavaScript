c70ace86d43e1d0f97bd302df53344e0e4f3d497
Carlos Rodriguez <crbotello08@gmail.com>
2024-10-21 10:13:22 +0200

    ejercicio9_arreglado

--- arrays/ej_9.js
+++ arrays/ej_9.js
@@ -1,43 +1,35 @@
 'use strict';
 let cuentas=[];
+let total=0;
 do{
-    let numeroCuenta = Number(prompt("Introduce el número de la cuenta (negativo para terminar)"));
-    if (numeroCuenta < 0) break; // Salir si el número de cuenta es negativo
+    cuentas["numero"] = Number(prompt("Introduce el número de la cuenta (negativo para terminar)"));
+    if (cuentas["numero"] < 0) break;
 
-    let nombreCliente = prompt("Introduce el nombre del cliente");
-    let saldo = Number(prompt("Introduce el saldo de la cuenta"));
+    cuentas["nombre"] = prompt("Introduce el nombre del cliente");
+    cuentas["saldo"] = Number(prompt("Introduce el saldo de la cuenta"));
 
-    // Guardamos la cuenta en un objeto
-    let cuenta = {
-        numeroCuenta: numeroCuenta,
-        nombreCliente: nombreCliente,
-        saldo: saldo
-    };
-    cuentas.push(cuenta);
+    total=operaciones(cuentas,total);
 }while(true);
 
-operaciones(cuentas);
+document.write("Total de los acreedores: "+total+"<br/>");
 
-function operaciones(cuentas) {
 
-    let total=0;
-
-    for (let i = 0; i < cuentas.length; i++) {
-        let cuenta= cuentas[i];
-        document.write("Numero de la cuenta: "+cuenta.numeroCuenta+"<br/>");
-        document.write("Nombre del cliente de la cuenta: "+cuenta.nombreCliente+"<br/>");
-        
-        if (cuenta.saldo>0) {
-            document.write("El cliente es un acreedor<br/>");
-            total+=cuenta.saldo;
-        } else {
-            if (cuenta.saldo<0) {
-                document.write("El cliente es un deudor<br/>");
-            } else {
-                document.write("El cliente es nulo<br/>");
-            }
-        }
-    }
+function operaciones(cuentas,total) {
+    console.log("entro a la funcion");
     
-    document.write("Total de los acreedores: "+total+"<br/>");
+    document.write("Numero de la cuenta: "+cuentas["numero"]+"<br/>");
+    document.write("Nombre del cliente de la cuenta: "+cuentas["nombre"]+"<br/>");
+        
+    if (cuentas["saldo"]>0) {
+        document.write("El cliente es un acreedor<br/>");
+        total+=cuentas["saldo"];
+    } else {
+        if (cuentas["saldo"]<0) {
+            document.write("El cliente es un deudor<br/>");
+        } else {
+            document.write("El cliente es nulo<br/>");
+        }
+    } 
+    return total;
+    
 }
